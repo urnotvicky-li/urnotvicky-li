053a8c4a3cfb8a7d5c9c6b27743e6f323a983438
Suny Gao <gaos@newschool.edu>
2020-11-13 19:25:43 -0500

    updates

--- toy/javascript/global.js
+++ toy/javascript/global.js
@@ -1,19 +1,20 @@
 window.addEventListener("load", function() {
 
     document.body.addEventListener('mousemove', moveEyes);
-
-
+    var eyes = document.querySelectorAll('.eye');
+    
+   
     function moveEyes(e){
-            var eyes = document.querySelectorAll('.eye');
+        eyes.forEach(function(eye) {
+            var cursorX = e.x;
+            var cursorY = e.y;
 
-            eye.forEach(function(eye){
-
-            var x = (eye.getBoundingClientRect().left) + (eye.getBoundingClientRect().Width / 2);
+            var x = (eye.getBoundingClientRect().left) + (eye.getBoundingClientRect().width / 2);
             var y = (eye.getBoundingClientRect().top) + (eye.getBoundingClientRect().height / 2);
 
-            var radian = Math.atan2 (e.pageX - x, e.pageY - y);
+            var radian = Math.atan2(cursorX - x, cursorY - y);
             var rotation = (radian * (180 / Math.PI) * -1) +180;
-
+            
             eye.style.transform = 'rotate('+rotation+ 'deg)';
         });
     }
