cb18c6f30a73ccb52f71b6a824c1f7a41781c2d4
urnotvicky-li <lic170@newschool.edu>
2020-11-18 01:26:57 +0800

    web_add_toy

--- toy/javascript/global.js
+++ toy/javascript/global.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
 
     document.body.addEventListener('mousemove', moveEyes);
-<<<<<<< HEAD
+
 
     var eyes = document.querySelectorAll('.eye');
 
@@ -11,28 +11,18 @@ window.addEventListener("load", function() {
             var cursorX = e.x;
             var cursorY = e.y;
 
-=======
     var eyes = document.querySelectorAll('.eye');
     
-   
-    function moveEyes(e){
-        eyes.forEach(function(eye) {
-            var cursorX = e.x;
-            var cursorY = e.y;
 
->>>>>>> 053a8c4a3cfb8a7d5c9c6b27743e6f323a983438
             var x = (eye.getBoundingClientRect().left) + (eye.getBoundingClientRect().width / 2);
             var y = (eye.getBoundingClientRect().top) + (eye.getBoundingClientRect().height / 2);
 
             var radian = Math.atan2(cursorX - x, cursorY - y);
-<<<<<<< HEAD
             var rotation = (radian * (180 / Math.PI) * -1) +270;
 
-=======
-            var rotation = (radian * (180 / Math.PI) * -1) +180;
+            var rotation = (radian * (180 / Math.PI) * -1) +270;
             
->>>>>>> 053a8c4a3cfb8a7d5c9c6b27743e6f323a983438
             eye.style.transform = 'rotate('+rotation+ 'deg)';
         });
     }
-});    
+});   
