ddd83947305c0090a1dd6a4d642659712757db12
urnotvicky-li <lic170@newschool.edu>
2021-03-03 01:48:40 +0800

    update 2

--- stories_as_networks/script/art4.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-	$(".diagonal-bg svg line").attr("stroke-width",  ((20 + scroll/10)  + "%"));
-	//30 is the starting width
-	//alter the amount of growth by changing scroll/x
-  console.log('hello')
-});
-
-// console.log('hello');
