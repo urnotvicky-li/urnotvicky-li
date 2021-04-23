100e768a61489906232b4a543c5e0254a3f8059e
urnotvicky-li <lic170@newschool.edu>
2021-04-23 21:05:54 +0800

    final

---
+++ lab_function_recipes/javascript/index.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $(slideToggle).click(() => {
+        $("#panel").slideToggle("slow");
+    });
+    $(slideToggle2).click(() => {
+        $("#panel2").slideToggle("slow");
+    });
+    $(slideToggle3).click(() => {
+        $("#panel3").slideToggle("slow");
+    });
+});
