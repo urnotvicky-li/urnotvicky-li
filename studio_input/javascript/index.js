1931d48c7a1caab5e85d2a9f5069fa6453915cee
urnotvicky-li <lic170@newschool.edu>
2021-05-05 05:58:20 +0800

    plan2_plusdrag

--- studio_input/javascript/index.js
+++ studio_input/javascript/index.js
@@ -23,7 +23,7 @@ function randomEmoji() {
 const emojiList = [
     {
         emoji: '😢',
-        description: '	嘿嘿'
+        description: '	困'
 
     },
     {
@@ -165,77 +165,6 @@ clickable.addEventListener("click", function(e) {
 
 // drag function
 
-
-// darg without library
-// var dragItem = document.querySelector('emoji-container');
-// var container = document.querySelector('container');
-
-// var active = false;
-// var currentX;
-// var currentY;
-// var initialX;
-// var initialY;
-// var xOffset = 0;
-// var yOffset = 0;
-
-//   container.addEventListener("touchstart", dragStart, false);
-//   container.addEventListener("touchend", dragEnd, false);
-//   container.addEventListener("touchmove", drag, false);
-
-//   container.addEventListener("mousedown", dragStart, false);
-//   container.addEventListener("mouseup", dragEnd, false);
-//   container.addEventListener("mousemove", drag, false);
-
-  // function dragStart(e) {
-  //   if (e.type === "touchstart") {
-  //     initialX = e.touches[0].clientX - xOffset;
-  //     initialY = e.touches[0].clientY - yOffset;
-  //   } else {
-  //     initialX = e.clientX - xOffset;
-  //     initialY = e.clientY - yOffset;
-  //   }
-
-  //   if (e.target === dragItem) {
-  //     active = true;
-  //   }
-  // }
-
-  // function dragEnd(e) {
-  //   initialX = currentX;
-  //   initialY = currentY;
-
-  //   active = false;
-  // }
-
-  // function drag(e) {
-  //   if (active) {
-    
-  //     e.preventDefault();
-    
-  //     if (e.type === "touchmove") {
-  //       currentX = e.touches[0].clientX - initialX;
-  //       currentY = e.touches[0].clientY - initialY;
-  //     } else {
-  //       currentX = e.clientX - initialX;
-  //       currentY = e.clientY - initialY;
-  //     }
-
-  //     xOffset = currentX;
-  //     yOffset = currentY;
-
-  //     setTranslate(currentX, currentY, dragItem);
-  //   }
-  // }
-
-//   // function setTranslate(xPos, yPos, el) {
-//   //   el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-//   // }
-
-//   var dragBox = function (drag, wrap) {
-
-
-
-//  dragBox(document.querySelector("#bar"), document.querySelector("#box"));
 
 // clear emoji button 
 function myFunction() {
